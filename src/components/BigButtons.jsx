d30b03bc70be53adaee624a9d003ee368a7098f6
Alan <alkalinemusic@gmail.com>
2024-04-19 21:29:02 -0400

    center and style buttons and text

--- src/components/BigButtons.jsx
+++ src/components/BigButtons.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { Button, Flex } from "antd";
+import { Button } from "antd"; // Importing Button from Ant Design
+import "../CSS/BigButtonsStyles.css"; // Ensure to import a CSS file for styles
 
 export default function BigButtons() {
   return (
-    <div>
-      BigButtons
-      <Flex gap="small" wrap="wrap">
-        <Button className="primary-home-btn" type="primary">
-          I am looking for an event
-        </Button>
-        <Button className="primary-home-btn" type="primary">
-          I want to post an event
-        </Button>
-      </Flex>
+    <div className="button-container">
+      <Button className="primary-home-btn" type="primary">
+        I am looking for an event
+      </Button>
+      <Button className="primary-home-btn" type="primary">
+        I want to post an event
+      </Button>
     </div>
   );
 }
